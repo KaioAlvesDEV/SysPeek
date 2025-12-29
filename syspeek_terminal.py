102e6f0c659458528114a624b2a19cca426c8e4f
KaioAlvesDEV <157232955+KaioAlvesDEV@users.noreply.github.com>
2025-12-29 09:30:15 -0300

    syspeek_terminal.py colors update

--- syspeek_terminal.py
+++ syspeek_terminal.py
@@ -1,8 +1,12 @@
+# NOTE: This entire code is temporary and will be completely rewritten later.
+
 def main():
     
-    from rich import print
+    from rich import console
     from syspeeklib import simple_pc_infos
 
+    console = console.Console()
+    
     # Retrieve simple operating system information
     computer_infos = {'OS': simple_pc_infos.get_simple_os_info(), 
                       'CPU': simple_pc_infos.get_simple_cpu_info()}
@@ -14,8 +18,8 @@ def main():
     # Display CPU brand
     cpu_brand = computer_infos['CPU']['Brand']
     
-    print(f'[bold blue]Operating System:[/bold blue][yellow] {os_name}[/yellow]')
-    print(f'[bold blue]Processor:[/bold blue][yellow] {cpu_brand}[/yellow]')
+    console.print(f'Operating System:', style=f'bold white on blue', end=''); console.print('', os_name, style='yellow')
+    console.print(f'Processor:', style=f'bold white on blue', end=''); console.print('', cpu_brand, style='yellow')
     input("\nPress Enter to exit...")
 
 if __name__ == "__main__":
