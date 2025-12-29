7b4550b0aa23da15748d0ddb7d10f80cf78004c3
KaioAlvesDEV <157232955+KaioAlvesDEV@users.noreply.github.com>
2025-12-29 09:11:27 -0300

    Added basic CPU information
    syspeeklib\simple_pc_infos.py:
    
    Now functions explicitly show what they return.
    
    Added function get_simple_cpu_info that returns a dictionary with the key 'Brand'.
    
    syspeek_terminal.py:
    
    Added the 'CPU' key in computer_infos for more organized use in the program.
    
    Variable cpu_brand to obtain the CPU brand string.
    
    Displays the CPU brand.
    
    Improved colors in the program.

--- syspeek_terminal.py
+++ syspeek_terminal.py
@@ -1,16 +1,21 @@
 def main():
+    
     from rich import print
     from syspeeklib import simple_pc_infos
 
     # Retrieve simple operating system information
-    computer_infos = {'OS': simple_pc_infos.get_simple_os_info()}
+    computer_infos = {'OS': simple_pc_infos.get_simple_os_info(), 
+                      'CPU': simple_pc_infos.get_simple_cpu_info()}
 
     # Display name and release of the operating system
     os_infos = computer_infos['OS']
     os_name = f"{os_infos['System']} {os_infos['Release']}"
-
-    # Print the operating system information using rich formatting
-    print(f'[bold yellow]Operating System:[/bold yellow][green] {os_name}[/green]')
+    
+    # Display CPU brand
+    cpu_brand = computer_infos['CPU']['Brand']
+    
+    print(f'[bold blue]Operating System:[/bold blue][yellow] {os_name}[/yellow]')
+    print(f'[bold blue]Processor:[/bold blue][yellow] {cpu_brand}[/yellow]')
     input("\nPress Enter to exit...")
 
 if __name__ == "__main__":
--- syspeeklib/simple_pc_infos.py
+++ syspeeklib/simple_pc_infos.py
@@ -1,6 +1,6 @@
 import platform
 
-def get_simple_os_info():
+def get_simple_os_info() -> dict:
     
     """Retrieve basic operating system information.
 
@@ -13,7 +13,22 @@ def get_simple_os_info():
     # Get system and release information
     simple_os_info = {
         'System': platform.system(),
-        'Release': platform.release(),
+        'Release': platform.release()
     }
     
     return simple_os_info
+
+def get_simple_cpu_info() -> dict:
+    
+    """Retrieve basic CPU information.
+
+    Returns:
+        dict: A dictionary containing the CPU brand.
+    """
+    
+    import cpuinfo 
+    
+    simple_cpu_info = {
+        'Brand': cpuinfo.get_cpu_info().get('brand_raw', 'Unknown'),}
+    
+    return simple_cpu_info
